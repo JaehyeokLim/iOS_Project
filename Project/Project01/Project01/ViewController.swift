f26169802b41a836cf8d748607ce4b93b9b2b9b3
jaehyeoklim <jaehyeoklim99@gmail.com>
2022-07-28 19:56:22 +0900

    특정시간마다 모비우스 서버에 정해진 컨테이너안에 데이터 업로드 구현

--- Project/Project01/Project01/ViewController.swift
+++ Project/Project01/Project01/ViewController.swift
@@ -22,15 +22,31 @@ class ViewController: UIViewController {
     var rotArray: [String] = []
     var preArray: [String] = []
     
+//    var accMobiusList: String = ""
+//    var rotMobiusList: String = ""
+//    var preMobiusList: String = ""
+    
+    let sensorNameArray: [String] = ["mAcc", "mGyr", "mPre"]
+    
     var accList: String = ""
     var rotList: String = ""
     var preList: String = ""
     
     var count: Int = 1
     
+    let mainLabel: UILabel = {
+        let mainLabel = UILabel()
+        
+        mainLabel.text = "Ready"
+        mainLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        mainLabel.textColor = UIColor.systemBlue
+        mainLabel.textAlignment = .center
+        
+        return mainLabel
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        Timer.scheduledTimer(timeInterval: 900, target: self, selector: #selector(ViewController.aaa), userInfo: nil, repeats: true)
         configureLayout()
         createCSV()
     }
@@ -52,10 +68,19 @@ class ViewController: UIViewController {
         }
         
         button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        
+        view.addSubview(mainLabel)
+        
+        mainLabel.snp.makeConstraints { make in
+            make.top.equalTo(view).offset(100)
+            make.width.equalTo(view)
+        }
     }
     
     @objc func buttonAction(_ sender: UIButton) {
         motionManagerInit()
+        Timer.scheduledTimer(timeInterval: 900, target: self, selector: #selector(ViewController.aaa), userInfo: nil, repeats: true)
+        mainLabel.text = "Start!"
         print("start")
     }
     
@@ -102,7 +127,7 @@ class ViewController: UIViewController {
 
         } else {
         
-            sendToData(array: accArray, time: currentDate, caseType: "Acceleration")
+            sendToData(array: accArray, time: currentDate, caseType: "mAcc")
             accArray.removeAll()
         }
     }
@@ -117,7 +142,7 @@ class ViewController: UIViewController {
             
         } else {
         
-            sendToData(array: rotArray, time: currentDate, caseType: "Rotation")
+            sendToData(array: rotArray, time: currentDate, caseType: "mGyr")
             rotArray.removeAll()
         }
     }
@@ -129,50 +154,52 @@ class ViewController: UIViewController {
 
         if preArray.count >= 1 {
             
-            sendToData(array: preArray, time: currentDate, caseType: "Pressure")
+            sendToData(array: preArray, time: currentDate, caseType: "mPre")
             preArray.removeAll()
         }
     }
     
     func sendToData(array: [String], time: Date, caseType: String) {
         
-        if caseType == "Acceleration" {
+        if caseType == "mAcc" {
             
             accList += String(format: "%.0f", time.timeIntervalSince1970)
-            
+
             for i in 0..<array.count {
                 accList += "," + array[i]
             }
             
             accList += "\n"
             
-        } else if caseType == "Rotation" {
+        } else if caseType == "mGyr" {
             
             rotList += String(format: "%.0f", time.timeIntervalSince1970)
-            
+
             for i in 0..<array.count {
                 rotList += "," + array[i]
             }
             
             rotList += "\n"
             
-        } else if caseType == "Pressure" {
+        } else if caseType == "mPre" {
             
             preList += String(format: "%.0f", time.timeIntervalSince1970)
-            
+
             preList += "," + array[0]
-            
+
             preList += "\n"
         }
     }
     
     @objc func aaa() {
-        writeCSV(sensorData: accList, caseType: "Acceleration", index: count)
-        writeCSV(sensorData: rotList, caseType: "Rotation", index: count)
-        writeCSV(sensorData: preList, caseType: "Pressure", index: count)
+        writeCSV(sensorData: accList, caseType: "mAcc", index: count)
+        writeCSV(sensorData: rotList, caseType: "mGyr", index: count)
+        writeCSV(sensorData: preList, caseType: "mPre", index: count)
+        
+        readFile(fileNumber: count)
         
         count += 1
-        
+
         accList = ""; rotList = ""; preList = ""
     }
     
@@ -212,6 +239,60 @@ class ViewController: UIViewController {
             catch let error as NSError {
                 print("CSV파일 생성 에러: \(error)")
             }
+    }
+    
+    func csvDataPostToMobius(csvData: String, conName: String) {
+        let semaphore = DispatchSemaphore (value: 0)
+
+        let parameters = "{\n    \"m2m:cin\": {\n        \"con\": \"\(csvData)\"\n    }\n}"
+        let postData = parameters.data(using: .utf8)
+
+        var request = URLRequest(url: URL(string: "http://114.71.220.59:7579/Mobius/S998/mobile/\(conName)")!,timeoutInterval: Double.infinity)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("12345", forHTTPHeaderField: "X-M2M-RI")
+        request.addValue("SIWLTfduOpL", forHTTPHeaderField: "X-M2M-Origin")
+        request.addValue("application/vnd.onem2m-res+json; ty=4", forHTTPHeaderField: "Content-Type")
+
+        request.httpMethod = "POST"
+        request.httpBody = postData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard data != nil else {
+              print(String(describing: error))
+              semaphore.signal()
+              return
+          }
+//            print(String(data: data, encoding: .utf8)!)
+            print("\(conName) Data is served.")
+            semaphore.signal()
+        }
+
+        task.resume()
+        semaphore.wait()
+    }
+    
+    func readFile(fileNumber: Int) {
+        
+        for fileName in sensorNameArray {
+            let fileManager = FileManager.default
+            
+            let folderName = "CSVFolder"
+            let csvFileName = "\(fileName)_\(fileNumber).csv"
+            
+            let documentUrl = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+            let directoryUrl = documentUrl.appendingPathComponent(folderName)
+            let fileUrl = directoryUrl.appendingPathComponent(csvFileName)
+            
+            do {
+                let dataFromPath: Data = try Data(contentsOf: fileUrl) // URL을 불러와서 Data타입으로 초기화
+                let text: String = String(data: dataFromPath, encoding: .utf8) ?? "문서없음" // Data to String
+                let data = text.replacingOccurrences(of: "\n", with: "")
+                csvDataPostToMobius(csvData: data, conName: fileName)
+                
+            } catch let e {
+                print(e.localizedDescription)
+            }
+        }
     }
 }
 
